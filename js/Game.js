13518fac4b7988630f61948ce66111ba723ca93a
ArjunItankar <86589224+ArjunItankar@users.noreply.github.com>
2022-07-10 12:05:39 +1000

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,46 @@
+class Game {
+  constructor() {
+    this.resetTitle = createElement("h2");
+    this.resetButton = createButton("");
+
+    this.leaderboardTitle = createElement("h2");
+
+    this.leader1 = createElement("h2");
+    this.leader2 = createElement("h2");
+  }
+
+  getState() {
+    var gameStateRef = database.ref("gameState");
+    gameStateRef.on("value", function(data) {
+      gameState = data.val();
+    });
+  }
+  update(state) {
+    database.ref("/").update({
+      gameState: state
+    })
+  }
+
+  start() {
+    player = new Player();
+    playerCount = player.getCount();
+
+    form = new Form();
+    form.display();
+
+    car1 = createSprite(width / 2 - 50, height - 100);
+    car1.addImage("car1", car1_img);
+    car1.scale = 0.07;
+
+    car2 = createSprite(width/2 + 100, height - 100);
+    car2.addImage("car2", car2_img);
+    car2.scale = 0.08;
+
+    cars = [car1, car2];
+
+    fuels = new Group();
+    powerCoins = new Group();
+
+    obstacles = newGroup();
+  }
+}
